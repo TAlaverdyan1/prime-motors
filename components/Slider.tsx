c237bc8f93b7b518295c8ce8d31b2c9036c5a496
Tigranuhi-A <tigranuhi.alaverdyan@gmail.com>
2023-12-12 21:29:36 +0400

    sliderStyle

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Link from "next/link";
+import Image from "next/image";
 import { useState, useEffect } from "react";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import { slider } from "@/lib/_data";
@@ -51,8 +52,8 @@ const Slider = () => {
                 currentSlider.map((el: SliderItem) => (
                     <div key={el.id}
                         className={` absolute w-[100%] h-[100vh] text-center flex justify-center items-center gap-6 object-cover flex-col bg-cover bg-no-repeat bg-center bg-fixed
-                        ${currentSlide === el.id ? " opacity-100" : " opacity-0"}`}
-                        style={{ backgroundImage: `url(${el.image})` }}>
+                        ${currentSlide === el.id ? " opacity-100" : " opacity-0"}`}>
+                        <Image src={el.image} alt="sliderImg" fill className="object-cover w-full h-full" />
                         <div className=" text-[1.5rem] px-4 text-white font-bold md:px-6 md:text-[2rem] lg:px-24 lg:text-[3rem]">{el.title}</div>
                         <Link href={el.route} className=" p-3 bg-red text-white font-bold md:px-8 md:py-4 md:text-[20px]">{el.btn_text}</Link>
                     </div>
