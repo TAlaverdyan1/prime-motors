f322bedbd05a1a50ec746ef1408cd06df5f6964c
Tigranuhi-A <tigranuhi.alaverdyan@gmail.com>
2024-01-04 20:16:44 +0400

    GLOBAL-CHANGESSTYLESforButtons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next';
-import { Montserrat } from 'next/font/google';
 import './globals.css';
 import { Header } from '../components/Header';
 import { Footer } from '../components/Footer';
@@ -9,14 +8,9 @@ export const SITE_SUFFIX = ' | Prime Motors';
 export const metadata: Metadata = {
   title: 'Prime Motors',
   icons: {
-    icon: "/favicon.ico?v=1"
+    icon: '/favicon.ico?v=1'
   }
 }
-
-const montserrat = Montserrat({
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700', '800', '900']
-});
 
 
 export default function RootLayout({
